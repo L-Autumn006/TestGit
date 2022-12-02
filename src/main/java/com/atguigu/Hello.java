8abd0ff8e88d350d0a1ad20cbc5eadbfe3591967
L-Autumn <353193098@qq.com>
2022-12-02 14:56:56 +0800

    进行一次本地Git提交测试

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+/**
+ * @BelongsProject: TestGit
+ * @BelongsPackage: com.atguigu
+ * @Author: chengqiushi
+ * @CreateTime: 2022-12-02 14:54
+ * @Description:
+ * @Version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
